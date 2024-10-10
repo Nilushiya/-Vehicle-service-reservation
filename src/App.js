ca16f70f871e7044e300e991b495cd642a94a592
Nilushiya <nilushiyak@gmail.com>
2024-10-10 19:06:01 +0530

    Mysql Connect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Profile from './components/Profile';
-import Reservations from './components/Reservations';
-import ReservationForm from './components/ReservationForm';
 import './App.css';
+import Login from './component/Login';
+import Reservations from './component/ReservationList';
+import Profile from './component/Profile';
+import ReservationForm from './component/ReservationForm';
 
 const App = () => {
   return (
