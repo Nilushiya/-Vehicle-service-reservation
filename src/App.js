a93afb4741860dbd46ac4d5351c8adc53afa699e
Nilushiya <nilushiyak@gmail.com>
2024-10-10 13:11:21 +0530

    Frontend components , Router create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Reservations from './components/Reservations';
+import ReservationForm from './components/ReservationForm';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path=''/>
-      </Routes>
-      </BrowserRouter>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Vehicle Service Reservation</h1>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/reservations" element={<Reservations />} />
+          <Route path="/reservations/new" element={<ReservationForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
